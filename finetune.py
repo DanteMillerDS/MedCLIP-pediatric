648c7fa3c5893370e5045302a10576780703549d
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-25 18:14:46 -0500

    Fix batch size bug

--- finetune.py
+++ finetune.py
@@ -1,4 +1,5 @@
 import torch
+from torch import optim
 import clip
 import os
 from medclip import MedCLIPModel, MedCLIPVisionModelViT, MedCLIPVisionModel
@@ -10,10 +11,12 @@ if status == 0:
 else:
     print("Error executing the command")
 torch._dynamo.config.suppress_errors = True
+def convert_models_to_fp32(model): 
+    for p in model.parameters(): 
+        p.data = p.data.float() 
+        p.grad.data = p.grad.data.float() 
+def train_clip(medical_type, generators, lengths):
 
-def train_clip(medical_type, batch_size train_generator, validation_generator, train_length, validation_length):
-    steps_per_epoch_training = train_length // batch_size
-    steps_per_epoch_validation = validation_length // batch_size
     device = "cuda" if torch.cuda.is_available() else "cpu"
     for vision_model_name,vision_model in zip(["MedCLIPVisionModelViT"],[MedCLIPVisionModelViT]):
         model = MedCLIPModel(vision_cls=vision_model)
@@ -27,63 +30,18 @@ def train_clip(medical_type, batch_size train_generator, validation_generator, t
     def evaluation():
 
 def train_medclip(medical_type, batch_size, train_generator, validation_generator, train_length, validation_length):
-    steps_per_epoch_training = train_length // batch_size
-    steps_per_epoch_validation = validation_length // batch_size
+ 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32",device=device,jit=False) #Must set jit=False for training
     
     def train():
-        
-    def evaluation():
-    
-    # Latest Update : 18 July 2022, 09:55 GMT+7
-
-# TO ADD :
-# Gradient Checkpointing
-# Filter out bias from weight decay
-# Decaying learning rate with cosine schedule
-# Half-precision Adam statistics
-# Half-precision stochastically rounded text encoder weights were used
-
-#BATCH_SIZE must larger than 1
-
-device = "cuda:0" if torch.cuda.is_available() else "cpu" # If using GPU then use mixed precision training.
-model, preprocess = clip.load("ViT-B/32",device=device,jit=False) #Must set jit=False for training
-
-class image_title_dataset(Dataset):
-    def __init__(self, list_image_path,list_txt):
-
-        self.image_path = list_image_path
-        self.title  = clip.tokenize(list_txt) #you can tokenize everything at once in here(slow at the beginning), or tokenize it in the training loop.
-
-    def __len__(self):
-        return len(self.title)
-
-    def __getitem__(self, idx):
-        image = preprocess(Image.open(self.image_path[idx])) # Image from PIL module
-        title = self.title[idx]
-        return image,title
-
-# use your own data
-list_image_path = ['folder/image1.jpg','folder2/image2.jpg'] 
-list_txt = ['description for image1.jpg' , 'description for image2.jpg']
-dataset = image_title_dataset(list_image_path,list_txt)
-train_dataloader = DataLoader(dataset,batch_size = BATCH_SIZE) #Define your own dataloader
-
-#https://github.com/openai/CLIP/issues/57
-def convert_models_to_fp32(model): 
-    for p in model.parameters(): 
-        p.data = p.data.float() 
-        p.grad.data = p.grad.data.float() 
-
-
-if device == "cpu":
+        if device == "cpu":
   model.float()
 else :
   clip.model.convert_weights(model) # Actually this line is unnecessary since clip by default already on float16
 
-loss_img = nn.CrossEntropyLoss()
-loss_txt = nn.CrossEntropyLoss()
+loss_img = torch.CrossEntropyLoss()
+loss_txt = torch.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=5e-5,betas=(0.9,0.98),eps=1e-6,weight_decay=0.2) #Params used from paper, the lr is smaller, more safe for fine tuning to new dataset
 
 # add your own code to track the training progress.
@@ -108,3 +66,54 @@ for epoch in range(EPOCH):
         convert_models_to_fp32(model)
         optimizer.step()
         clip.model.convert_weights(model)
+        
+    def evaluation():
+        def zero_shot_classification(model, image_batch,categories):
+        
+       
+        text_inputs = torch.cat([clip.tokenize(f"a photo of {c} lungs.") for c in categories]).to(device)
+        with torch.no_grad():
+            image_features = model.encode_image(image_batch)
+            text_features = model.encode_text(text_inputs)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+            top_probs, top_labels = similarity.topk(1, dim=-1)
+        return top_probs, top_labels
+
+    def evaluate(model, data_loaders, device, steps, data_types,categories):
+        model.eval()
+        y_true, y_pred, y_score = [], [], []
+        with torch.no_grad():
+            for data_loader, data_type, step in zip(data_loaders, data_types, steps):
+                for step_ind in tqdm(range(step), desc=f'Evaluate {data_type}'):
+                    inputs, labels = data_loader[step_ind][0], data_loader[step_ind][1]
+                    inputs = torch.from_numpy(inputs).to(device).permute(0, 3, 1, 2)
+                    labels = torch.from_numpy(labels).to(device).float().unsqueeze(1)
+                    top_probs, top_labels = zero_shot_classification(model,inputs, categories)
+                    y_true.extend(labels.cpu().numpy())
+                    y_pred.extend(top_labels.cpu().numpy())
+                    y_score.extend(top_probs.cpu().numpy())
+        return accuracy_score(y_true, y_pred), precision_score(y_true, y_pred), recall_score(y_true, y_pred), roc_auc_score(y_true, y_score), classification_report(y_true, y_pred), np.array2string(confusion_matrix(y_true, y_pred))
+
+    acc, prec, rec, auc, cr, cm = evaluate(clip_model, generators,
+                               device, steps, ["Test"],
+                               categories)
+    print(f"\nAccuracy: {acc:.4f}, Precision: {prec:.4f}, Recall: {rec:.4f}, AUC: {auc:.4f}")
+    directory = f"{medical_type}/clip"
+    filename = "classification_results.txt"
+    filepath = os.path.join(directory, filename)
+    os.makedirs(directory, exist_ok=True)
+    with open(filepath, "w") as file:
+        file.write(f"Accuracy: {acc:.4f}\n")
+        file.write(f"Precision: {prec:.4f}\n")
+        file.write(f"Recall: {rec:.4f}\n")
+        file.write(f"AUC: {auc:.4f}\n")
+        file.write('Classification Report\n\n{}\n\nConfusion Matrix\n\n{}\n'.format(cr, cm))
+    print(f"Results saved to {filepath}")
+        
+
+
+
+
+
--- run_model.py
+++ run_model.py
@@ -11,21 +11,21 @@ if __name__ == "__main__":
     batch_size = 256
     generators, lengths = load_data.create_loader(medical_type,batch_size,model_type)
     visualize.save_random_images_from_generators(generators,[medical_type,model_type],2)
-    zero_shot.run_zero_shot_classification_medclipmodel(medical_type, batch_size, generators, lengths)
+    zero_shot.run_zero_shot_classification_medclipmodel(medical_type, generators, lengths)
     
     medical_type = 'ori'  
     generators, lengths = load_data.create_loader(medical_type,batch_size,model_type)
     visualize.save_random_images_from_generators(generators,[medical_type,model_type],2)
-    zero_shot.run_zero_shot_classification_medclipmodel(medical_type, batch_size, generators, lengths)
+    zero_shot.run_zero_shot_classification_medclipmodel(medical_type, generators, lengths)
     
     medical_type = 'ucsd'  
     model_type = "clip"
     batch_size = 256
     generators, lengths = load_data.create_loader(medical_type,batch_size,model_type)
     visualize.save_random_images_from_generators(generators,[medical_type,model_type],2)
-    zero_shot.run_zero_shot_classification_clipmodel(medical_type, batch_size, generators, lengths)
+    zero_shot.run_zero_shot_classification_clipmodel(medical_type, generators, lengths)
     
     medical_type = 'ori'  
     generators, lengths = load_data.create_loader(medical_type,batch_size,model_type)
     visualize.save_random_images_from_generators(generators,[medical_type,model_type],2)
-    zero_shot.run_zero_shot_classification_clipmodel(medical_type, batch_size, generators, lengths)
+    zero_shot.run_zero_shot_classification_clipmodel(medical_type, generators, lengths)
