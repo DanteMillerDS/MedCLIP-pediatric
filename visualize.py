0a132aac18314f3df14ab1954e0c50394e471b34
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-24 22:41:31 -0500

    Fix Visualization Issue

--- visualize.py
+++ visualize.py
@@ -2,32 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+
 def save_random_images_from_generators(train_generator, valid_generator, test_generator, info, num_images=2):
     medical_type,model_type = info
     directory = os.path.join(medical_type, model_type, "images")
     filename = "cxr_images.png"
     filepath = os.path.join(directory, filename)
     os.makedirs(directory, exist_ok=True)
-    train_images, _ = next(train_generator)
-    valid_images, _ = next(valid_generator)
-    test_images, _ = next(test_generator)
     plt.figure(figsize=(12, 8))
-    for i in range(num_images):
-        idx = np.random.randint(0, train_images.shape[0])
-        plt.subplot(3, num_images, i + 1)
-        plt.imshow(train_images[idx])
-        plt.title("Train")
-        plt.subplot(3, num_images, num_images + i + 1)
-        plt.imshow(valid_images[idx])
-        plt.title("Validation")
-        plt.subplot(3, num_images, 2 * num_images + i + 1)
-        plt.imshow(test_images[idx])
-        plt.title("Test")
+    for gen, title in zip([train_generator, valid_generator, test_generator], ["Train", "Validation", "Test"]):
+        images, _ = next(gen)  # Fetch a batch of images and labels
+        for i in range(num_images):
+            idx = np.random.randint(0, images.shape[0])  # Select a random index
+            image = images[idx]
+
+            if image.shape[0] < image.shape[-1]:  
+                image = image.transpose(1, 2, 0)  
+            plt.subplot(3, num_images, i + 1 + (["Train", "Validation", "Test"].index(title) * num_images))
+            plt.imshow(image)
+            plt.title(title)
     plt.tight_layout()
     plt.savefig(filepath)
-    print(f"Images saved to {filepath}")
+    print(f"Results saved to {filepath}")
     plt.close()
     train_generator.reset()
     valid_generator.reset()
     test_generator.reset()
     
+    
+    
+
