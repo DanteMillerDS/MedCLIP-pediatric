6899afdc75a526da057f46c1500e9c51253a2d65
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-26 20:43:12 -0500

    Fix medical type error

--- train_medclip.py
+++ train_medclip.py
@@ -1,7 +1,7 @@
 import data_loader.extract_data as extract_data
 import data_loader.load_data as load_data
 import visualize.visualize as visualize
-from fine_tune.finetune_clip import TrainClipClassifier
+from fine_tune.finetune_medclip import TrainMedClipClassifier
 def run_finetune_clip(medical_type, model_type, batch_size):
     """
     Performs fine-tuning of the CLIP model on a given medical type and model type.
@@ -11,8 +11,8 @@ def run_finetune_clip(medical_type, model_type, batch_size):
     """
     generators, lengths = load_data.create_loader(medical_type, batch_size, model_type)
     visualize.save_random_images_from_generators(generators, [medical_type, model_type, "fine_tune"], 2)
-    if model_type == "clip":
-        classifier = TrainClipClassifier(medical_type)
+    if model_type == "medclip":
+        classifier = TrainMedClipClassifier(medical_type)
         classifier.run(generators, lengths)
         return classifier
     else:
@@ -21,8 +21,8 @@ def run_finetune_clip(medical_type, model_type, batch_size):
 if __name__ == "__main__":
     extract_data.mount_and_process()
     batch_size = 256
-    model_types = ['clip']
+    model_types = ['medclip']
     medical_types = ['ucsd', 'ori']
-    ucsd_classifier = run_finetune_clip(medical_types[0], model_types[0], batch_size)
-    ori_classifier = run_finetune_clip(model_types[1], model_types[0], batch_size)
+    ucsd_classifier = run_finetune_clip(model_types[0], medical_types[0], batch_size)
+    ori_classifier = run_finetune_clip(model_types[0], medical_types[1], batch_size)
     
