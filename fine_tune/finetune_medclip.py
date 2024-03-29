ad589f08216164579e69a2d13bab31e57fc9f8fd
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-29 16:29:45 -0500

    Fix error for finetuning medclip

--- fine_tune/finetune_medclip.py
+++ fine_tune/finetune_medclip.py
@@ -13,7 +13,7 @@ from torch.cuda.amp import GradScaler, autocast
 import matplotlib.pyplot as plt
 
 class TrainMedClipClassifier:
-    def __init__(self, medical_type, epochs=25):
+    def __init__(self, medical_type, epochs=50):
         """
         Initializes the TrainMedClipClassifier with a specific medical type and computational device.
         """
