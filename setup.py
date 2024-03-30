e354c9facf5015a3f4da5185114a124e4b1b3471
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-29 19:39:42 -0500

    Fix absolute paths

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup,find_packages
-
-setup(
-    name='MedCLIP-pediatric',  # Ensure this line is present
-    version='0.1.0',
-    packages=find_packages(),
-    scripts=['scripts/t_clip.py', 'scripts/t_medclip.py', 'scripts/zs_clip_base.py', 'scripts/zs_medclip_base.py', 'scripts/zs_clip_finetune.py', 'scripts/zs_medclip_finetune.py'] 
-
-)
