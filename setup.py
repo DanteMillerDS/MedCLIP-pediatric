49cc24d64ab1382697d89070d2592685ac597e8d
Dante Miller <dantemillerpersonal@gmail.com>
2024-03-29 19:16:43 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
-    packages=['data_loader', 'fine_tune', 'visualize', 'zero_shot'],
+    name='MedCLIP-pediatric',  # Ensure this line is present
+    version='0.1.0',
+    packages=find_packages(),
     scripts=['scripts/t_clip.py', 'scripts/t_medclip.py', 'scripts/zs_clip_base.py', 'scripts/zs_medclip_base.py', 'scripts/zs_clip_finetune.py', 'scripts/zs_medclip_finetune.py'] 
+
 )
